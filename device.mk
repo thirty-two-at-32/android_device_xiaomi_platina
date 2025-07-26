f68b5c2ac786cb45ae3ca4279671966a0664d0d3
sabarop <sabar.pakpahan@gmail.com>
2025-07-26 08:05:08 +0700

    platina: Set aapt config

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom_ramdisk \
     init.device.rc
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
